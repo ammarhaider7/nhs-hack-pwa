37e15a980c52b8d1c9ed1b9887ddf95c00122c8f
Ammar Haider <ammar.haider@bjss.com>
2020-01-22 12:42:43 +0000

    Updates

---
+++ precache-manifest.1bfd0c16c803bee5328cad1c763dd162.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5f0208bd2d72a1a8cce5910a8d11a1a",
+    "url": "/nhs-hack-pwa/index.html"
+  },
+  {
+    "revision": "181aaae1e4d13825cd03",
+    "url": "/nhs-hack-pwa/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7faf9c4b59632dd6c7fe",
+    "url": "/nhs-hack-pwa/static/js/2.013271c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/nhs-hack-pwa/static/js/2.013271c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "181aaae1e4d13825cd03",
+    "url": "/nhs-hack-pwa/static/js/main.4e574fc7.chunk.js"
+  },
+  {
+    "revision": "6f92ac1c17c241443654",
+    "url": "/nhs-hack-pwa/static/js/runtime-main.903f7a33.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nhs-hack-pwa/static/media/logo.5d5d9eef.svg"
+  }
+]);
