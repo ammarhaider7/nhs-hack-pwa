2634d758a28888190643a441f58ee07db78fbad0
Ammar Haider <ammar.haider@bjss.com>
2020-01-22 13:07:38 +0000

    fixes build

--- src/App.js
+++ src/App.js
@@ -1,45 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import DeviceOrientation from 'react-device-orientation';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <DeviceOrientation>
-          {({ absolute, alpha, beta, gamma }) => (
-            <div>
+class App extends Component {
+
+  formatAngleData(val) {
+    return Number(val).toFixed(2);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <DeviceOrientation>
+            {({ absolute, alpha, beta, gamma }) => (
               <div>
-                {`Absolute: ${absolute.toFixed(2)}`}
+                <div>
+                  {`Absolute: ${this.formatAngleData(absolute)}`}
+                </div>
+                <div>
+                  {`Alpha: ${this.formatAngleData(alpha)}`}
+                </div>
+                <div>
+                  {`Beta: ${this.formatAngleData(beta)}`}
+                </div>
+                <div>
+                  {`Gamma: ${this.formatAngleData(gamma)}`}
+                </div>
               </div>
-              <div>
-                {`Alpha: ${alpha.toFixed(2)}`}
-              </div>
-              <div>
-                {`Beta: ${beta.toFixed(2)}`}
-              </div>
-              <div>
-                {`Gamma: ${gamma.toFixed(2)}`}
-              </div>
-            </div>
-          )}
-        </DeviceOrientation>
-      </header>
-    </div>
-  );
+            )}
+          </DeviceOrientation>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
