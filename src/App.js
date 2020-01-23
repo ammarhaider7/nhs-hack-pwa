e82a50bc73320443b35316d22a6af9eb98c920d9
Ammar Haider <ammar.haider@bjss.com>
2020-01-23 13:40:00 +0000

    dark background

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
 import { Container, Box, Grid } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+
+import SimpleExpansionPanel from './components/simple-expansion-panel';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
 
 export default () => (
-  <Grid container spacing={2}>
-    <Container>
-      <Box mt={2}>
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
-      </Box>
-    </Container>
-  </Grid>
-
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Grid container spacing={2}>
+      <Container>
+        <Box mt={2}>
+          <Button variant="contained" color="primary">
+            Hello World
+            </Button>
+        </Box>
+        <Box mt={2}>
+          <SimpleExpansionPanel />
+        </Box>
+      </Container>
+    </Grid>
+  </ThemeProvider>
 )
+
